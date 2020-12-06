ba6413b3a3b237078e1f608d041cd17340850324
Ubuntu <ubuntu@ip-172-31-27-140.eu-central-1.compute.internal>
2020-12-06 15:07:44 +0000

    too many fixes

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export ROS_DOMAIN_ID=42
+sudo /usr/bin/X :0 & 
+export DISPLAY=:0
 source /opt/ros/dashing/setup.bash
 ros2 topic list 
 source /home/ubuntu/simulation-manager/Simulair-simulation-manager/venv/bin/activate
 sudo python3 /home/ubuntu/simulation-manager/Simulair-simulation-manager/app.py
+
 
